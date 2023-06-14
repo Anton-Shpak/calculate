d0e24a8b1ac8be934ca9a03ca99bbdd0f0ba3a46
Anton <perforattorov@gmail.com>
2023-06-14 20:53:46 +0300

    Test

---
+++ src/com/company/Package1/TestClass1.java
@@ -0,0 +1,12 @@
+package com.company.Package1;
+
+public class TestClass1 {
+
+    public static void main(String[] args) {
+        tasl1();
+    }
+
+private static void tasl1() {
+    System.out.println("Поехали");
+}
+}
