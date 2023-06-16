8f2426baf0f1f392513c58857465398d9f33e125
Anton-Shpak <perforattorov@gmail.com>
2023-06-16 14:07:59 +0300

    Зачатки калькулятора

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -1,10 +1,6 @@
 package com.company;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
-import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
