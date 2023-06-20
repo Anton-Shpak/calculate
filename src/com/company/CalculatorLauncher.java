7903d6d1daf8435bc2abd8cafe2effb7a7ac4dc0
Anton <perforattorov@gmail.com>
2023-06-20 20:23:22 +0300

    Предрелиз

--- src/com/company/CalculatorLauncher.java
+++ src/com/company/CalculatorLauncher.java
@@ -1,4 +1,3 @@
-import java.nio.channels.ScatteringByteChannel;
 import java.util.Scanner;
 
 public class CalculatorLauncher {
@@ -12,58 +11,57 @@ class Calculator {
     public void startCalculator() {
 //        Scanner scanner = new Scanner(System.in);
 //
-//        System.out.print("Для вычисления введите только арабские или только римские числа от 0 до 10, /n вставив между ними один из операторов * / + - ");
+//        System.out.print("Для вычисления введите пару арабских или пару римских чисел от 0 до 10, " +
+//                "вставив между ними один из операторов * / + - ");
 //        String input = scanner.nextLine();
-//        String input = "3 - IV";
+
+////      String input = "3 - IV";
 //        String input = "1 +2 +3";
-        String input = "11 / 0";
-
-
-        String result;
-        input = StringManipulator.removeSpacesAndSymbols(input);
+          String input = "XII/ X";
 
 
 
 
-        String[] parts = StringManipulator.splitInput(input);
-        //Порезать по оператору
 
-
-        if (parts.length != 2) { // Проверка на количество операндов
-            System.out.println("Ошибка: Некорректное количество чисел или операторов");
-            return;
+        try {
+            String result = calculateResult(input);
+            System.out.println("Результат: " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка: " + e.getMessage());
         }
-        char operator = input.charAt(parts[0].length());
+
+//        scanner.close();
+    }
+
+    private String calculateResult(String input) {
+        String inputWithoutSpacesAndSymbols = StringManipulator.removeSpacesAndSymbols(input);
+        String[] parts = StringManipulator.splitInput(inputWithoutSpacesAndSymbols);
+
+        if (parts.length != 2) {
+            throw new IllegalArgumentException("Некорректное количество чисел или операторов");
+        }
+
+        char operator = inputWithoutSpacesAndSymbols.charAt(parts[0].length());
 
         int num1, num2, sum;
-        if (StringManipulator.containsRomanNumerals(parts[0]) && StringManipulator.containsRomanNumerals(parts[1])) { //Если два операнда римские
-
+        if (StringManipulator.containsRomanNumerals(parts[0]) && StringManipulator.containsRomanNumerals(parts[1])) {
             num1 = NumberConverter.convertRomanToArabic(parts[0]);
-            num2 = NumberConverter.convertRomanToArabic(parts[1]); //Конвертируем
+            num2 = NumberConverter.convertRomanToArabic(parts[1]);
             sum = CalculatorEngine.calculate(num1, num2, operator);
-            if (sum > 0 ) {
-
-                result = NumberConverter.convertArabicToRoman(sum);
-            } else {
-                System.out.print("Ошибка. В римском счислении нет отрицательных чисел");
-                return;
+            if (sum < 0) {
+                throw new IllegalArgumentException("В римском счислении нет отрицательных чисел");
             }
+            return NumberConverter.convertArabicToRoman(sum);
         } else {
             try {
                 num1 = Integer.parseInt(parts[0]);
                 num2 = Integer.parseInt(parts[1]);
-                result = String.valueOf(CalculatorEngine.calculate(num1, num2, operator));
+                return String.valueOf(CalculatorEngine.calculate(num1, num2, operator));
             } catch (NumberFormatException e) {
-                System.out.println("Ошибка: Должна быть введена только пара арабских или пара римских цифр, каждое число не больше 10");
-                return;
+                throw new IllegalArgumentException("Должна быть введена только пара арабских или пара римских цифр, " +
+                        "каждое число не больше 10");
             }
         }
-
-        System.out.println("Результат: " + result);
-
-
-
-//        scanner.close();
     }
 }
 
@@ -72,19 +70,13 @@ class StringManipulator {
         return input.replaceAll("\\s+", "");
     }
 
-//    public static boolean containsOperator(String input) {
-//        return input.contains("+") || input.contains("-") || input.contains("*") || input.contains("/");
-//    }
-
     public static String[] splitInput(String input) {
-        if (input.contains("+") || input.contains("-") || input.contains("*") || input.contains("/")) {
+        if (input.matches(".*[+\\-*/].*")) {
             return input.split("[+\\-*/]");
         } else {
             throw new IllegalArgumentException("Некорректный оператор");
         }
     }
-
-
 
     public static boolean containsRomanNumerals(String input) {
         return input.matches(".*[IVX].*");
@@ -93,32 +85,41 @@ class StringManipulator {
 
 class NumberConverter {
     public static int convertRomanToArabic(String number) {
-
-        switch (number) {
-            case "I":
-                return 1;
-            case "II":
-                return 2;
-            case "III":
-                return 3;
-            case "IV":
-                return 4;
-            case "V":
-                return 5;
-            case "VI":
-                return 6;
-            case "VII":
-                return 7;
-            case "VIII":
-                return 8;
-            case "IX":
-                return 9;
-            case "X":
-                return 10;
-        }
-
-        return 11;
+    switch (number) {
+        case "I":
+            return 1;
+        case "II":
+            return 2;
+        case "III":
+            return 3;
+        case "IV":
+            return 4;
+        case "V":
+            return 5;
+        case "VI":
+            return 6;
+        case "VII":
+            return 7;
+        case "VIII":
+            return 8;
+        case "IX":
+            return 9;
+        case "X":
+            return 10;
+        default:
+            try {
+                int num = Integer.parseInt(number);
+                if (num > 0 && num <= 10) {
+                    return num;
+                } else {
+                    throw new IllegalArgumentException("Число должно быть от 1 до 10");
+                }
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("Некорректное римское число или число больше 10");
+            }
     }
+}
+
 
     public static String convertArabicToRoman(int num) {
         if (num > 0) {
@@ -136,17 +137,17 @@ class NumberConverter {
             }
             return sb.toString();
         } else {
-            return "Ошибка. В римском счислении нет отрицательных чисел";
+            throw new IllegalArgumentException("Ошибка. В римском счислении нет отрицательных чисел");
         }
     }
 }
 
 class CalculatorEngine {
     public static int calculate(int num1, int num2, char operator) {
-            if (num1 > 10 || num2 > 10) {
-                throw new IllegalArgumentException("Одно из введённых чисел больше 10");
-            }
-    try {
+        if (num1 > 10 || num2 > 10) {
+            throw new IllegalArgumentException("Одно из введённых чисел больше 10");
+        }
+
         switch (operator) {
             case '+':
                 return num1 + num2;
@@ -161,13 +162,7 @@ class CalculatorEngine {
                     throw new ArithmeticException("Деление на ноль");
                 }
             default:
-                throw new IllegalArgumentException("О Некорректный оператор");
+                throw new IllegalArgumentException("Некорректный оператор");
         }
-    } catch (Exception e) {
-        System.out.println("Ошибка: " + e.getMessage());
-        return Integer.MIN_VALUE;
     }
 }
-    }
-
-
